7f01296ff50b47b9ff15a7b350ccd8f73d7d1234
aa2503@bk.ru <aa2503@bk.ru>
2022-01-13 17:44:31 +0300

    Add project files.

---
+++ ChessGame.cpp
@@ -0,0 +1,12 @@
+﻿// ChessGame.cpp : Defines the entry point for the application.
+//
+
+#include "ChessGame.hpp"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
